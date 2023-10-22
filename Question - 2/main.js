6d3856a3efd48fa0ea78f4b66a3af7df0249bd96
abdulBasit099 <143632090+abdulBasit099@users.noreply.github.com>
2023-10-21 18:01:43 -0700

    Add files via upload

---
+++ Question - 2/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question - 2 
+Object.defineProperty(exports, "__esModule", { value: true });
+const fname = " Eric ";
+const message = `“Hello ${fname}, would you like to learn some Python today ? ”`;
+console.log(message);
---
+++ Question - 2/main.ts
@@ -0,0 +1,7 @@
+// Question - 2 
+
+const fname = " Eric ";
+
+const message = `“Hello ${fname}, would you like to learn some Python today ? ”`;
+
+console.log(message);
